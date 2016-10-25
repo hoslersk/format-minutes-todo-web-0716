8475aca315f28a7d8e0b977dc54a04b206ce106b
Schyler Hosler <schylerhosler@gmail.com>
2016-10-24 22:36:12 -0400

    Ruby practice with conversion of minutes to hours all tests passing

--- lib/format_minutes.rb
+++ lib/format_minutes.rb
@@ -1,3 +1,12 @@
 def time_formatter(minutes)
   # your code here
+  if minutes < 60
+    return "#{minutes} minute(s)"
+  elsif minutes % 60 == 0
+    return "#{minutes / 60} hour(s)"
+  else
+    hrs = minutes / 60
+    mins = minutes % 60
+    return "#{hrs} hour(s), #{mins} minute(s)"
+  end
 end
